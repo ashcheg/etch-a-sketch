3d5c73a0fc8cc01b89e5cb4f9748db12f21f2449
ashcheg <alina.shcheg@gmail.com>
2022-03-20 03:46:30 -0400

    creates simple grid 3x3

--- myScript.js
+++ myScript.js
@@ -1,11 +1,31 @@
 const container = document.querySelector(".container");
 
+/*
 function createGrid(num) {
     for (i=0; i<(num*num); i++) {
         const div = document.createElement('div');
         div.classList.add('box');
         div.textContent = `box ${i+1}`;
         container.appendChild(div);
+    }
+}
+*/
+
+function createGrid(num) {
+    // iteration over rows
+    for (i=0; i<num; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+
+        // iteration over columns
+        for (j=0; j<num; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.textContent = `col${j+1}-row${i+1}`;
+            row.appendChild(cell);
+        }
+        
+        container.appendChild(row);
     }
 }
 
