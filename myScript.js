8438c05e00c9783c7c70ae59ee43073ca043073d
ashcheg <alina.shcheg@gmail.com>
2022-03-22 17:35:07 -0400

    working flexible grid made with flexbox

--- myScript.js
+++ myScript.js
@@ -1,16 +1,5 @@
 const container = document.querySelector(".container");
 
-/*
-function createGrid(num) {
-    for (i=0; i<(num*num); i++) {
-        const div = document.createElement('div');
-        div.classList.add('box');
-        div.textContent = `box ${i+1}`;
-        container.appendChild(div);
-    }
-}
-*/
-
 function createGrid(num) {
     // iteration over rows
     for (i=0; i<num; i++) {
@@ -21,7 +10,7 @@ function createGrid(num) {
         for (j=0; j<num; j++) {
             const cell = document.createElement('div');
             cell.classList.add('cell');
-            cell.textContent = `col${j+1}-row${i+1}`;
+            // cell.textContent = `col${j+1}-row${i+1}`; 
             row.appendChild(cell);
         }
         
@@ -29,4 +18,4 @@ function createGrid(num) {
     }
 }
 
-createGrid(3);
+createGrid(16);
