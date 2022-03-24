108977a1dce80da8034f0783c73e84e60efa30a1
ashcheg <alina.shcheg@gmail.com>
2022-03-24 01:23:51 -0400

    added buttons and functions for creating new grid and random color

--- myScript.js
+++ myScript.js
@@ -2,7 +2,8 @@ const container = document.querySelector(".container");
 
 
 
-function createGrid(num) {
+function createGrid(e) {
+    num = prompt('enter number of rows/columns:');
     // iteration over rows
     for (i=0; i<num; i++) {
         const row = document.createElement('div');
@@ -18,11 +19,37 @@ function createGrid(num) {
         
         container.appendChild(row);
     }
+    const cells = Array.from(document.querySelectorAll(".cell"));
+    cells.forEach(cell => cell.addEventListener('mouseenter', function(event) {
+        event.target.classList.add('newColor');
+    }))
 }
 
-createGrid(16);
+function randomRGB(e) {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    const rgb = `rgb(${r},${g},${b})`;
+    const cells = Array.from(document.querySelectorAll(".cell"));
+    cells.forEach(cell => cell.addEventListener('mouseenter', function(e) {
+        e.target.style.backgroundColor = rgb;
+    }))
+}
 
-const cells = Array.from(document.querySelectorAll(".cell"));
-cells.forEach(cell => cell.addEventListener('mouseenter', function(event) {
-    event.target.classList.add('newColor');
-}))
+const newGridButton = document.querySelector('#newGrid');
+const randomRgbButton = document.querySelector('#randomColor');
+
+newGridButton.addEventListener('click', createGrid);
+randomRgbButton.addEventListener('click', randomRGB);
+
+/* Make modes: default and rainbow
+when button pressed => change mode function
+inside change mode function => calls to functions default and rainbow
+Function generating random rgb is separate and called for each cell
+
+Later add :
+1. button for choosing particular color;
+2. btton for adjusting translucency;
+
+Later: make color change only when mousedown AND mouseover
+*/
